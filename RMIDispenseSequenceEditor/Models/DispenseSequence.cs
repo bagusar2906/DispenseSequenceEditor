2e0cc4ba2de4074887ccabe0a0659268050769a4
Bagus Arjanto <bagusar@gmail.com>
2025-11-08 17:33:08 +0700

    fix:missing file

---
+++ RMIDispenseSequenceEditor/Models/DispenseSequence.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMIDispenseSequenceEditor.Models
+{
+  public class DispenseSequence
+  {
+    public string[] IngredientsName { get; set; }
+    public int Column { get; set; }
+  }
+}
